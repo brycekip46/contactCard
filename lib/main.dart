7054804543f2e2d5b8ed4f1f0c13ab50e4877d98
kiprutobrysonn <brycekip46@gmail.com>
2023-05-29 15:30:44 +0300

    Few updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'contacts.dart';
+import 'Contact_card.dart';
 
 void main() {
+
   runApp( const MaterialApp(
     home: MyApp(),));
 }
@@ -20,70 +22,42 @@ class _MyAppState extends State<MyApp> {
 
   ];
 
-  Widget contactTemplate(Contact contact){
-    return  Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage('assets/hacker.jpg'),
-              radius: 30.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    contact.name,
-                    style: const TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 6.0),
-                  Text(
-                    contact.email,
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  SizedBox(height: 6.0),
-                    Text(contact.phone,
-                      style: const TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey,
-                    ),
-                    )],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         title: Text("Contacts"),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.black87,
       ),
-      body: Column(
-        children: contacts.map((contact) => contactTemplate(contact)).toList(),
+      body:Container(
+        color: Colors.black,
+        child: Column(
+          children: contacts.map((contact) => Contact_card(contact: contact,
+          delete: (){
+            setState(() {
+              contacts.remove(contact);
+            });
+          },
+          )).toList(),
 
+
+        ),
       ),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-          child: const Icon(Icons.add),
+          onPressed: (){
+            setState(() {
+              contacts.add(Contact(name: "John Kiriamiti",
+                  email: "kiriamiti@gmail.com",
+                  phone: "1234567890"));
+
+            });},
           backgroundColor: Colors.blueAccent,
+          child: const Icon(Icons.add),
         )
 
     );
   }
 }
+
+
